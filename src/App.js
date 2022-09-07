39f919c13a62325515402b65d8fe2a3b298e5465
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-09-07 20:22:39 +0530

    added sidebar, navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Hero from "./Components/Hero/Hero";
+import Nav from "./Components/Navbar/Nav";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App static">
+      <Nav />
+      <div className="absolute top-16 left-0 flex border-2 border-purple-900 w-full">
+        <Sidebar />
+        <div className="main-content-container relative top-4 mx-8 w-full border-2 border-red-900">
+          <Hero/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero-section p-12 h-64 w-full border-2 bg-red-400">
+    </section>
+  );
+}
+
+export default Hero;
